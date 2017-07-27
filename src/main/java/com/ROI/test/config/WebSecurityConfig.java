189dd2563d44adaf3f77946fe27554aeed453ce6
bugogish <ludlap@yandex.ru>
2017-07-27 19:58:43 +0300

    added redirect to /ums page with placeholder

--- src/main/java/com/ROI/test/config/WebSecurityConfig.java
+++ src/main/java/com/ROI/test/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
-                .fullyAuthenticated().and().formLogin();
+                .fullyAuthenticated().and().formLogin().successForwardUrl("/ums");
     }
 
     @Configuration

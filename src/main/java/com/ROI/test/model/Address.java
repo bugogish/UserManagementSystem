7494149cc747219342e47bdd0c7551f078afc589
bugogish <ludlap@yandex.ru>
2017-07-27 16:10:36 +0300

    address added

--- src/main/java/com/ROI/test/model/Address.java
+++ src/main/java/com/ROI/test/model/Address.java
@@ -3,4 +3,19 @@ package com.ROI.test.model;
 //        Address is {zip, country, city, district, street}
 
 public class Address {
+    private Integer zip;
+
+    private String country;
+
+    private String city;
+
+    private String district;
+
+    private String street;
+
+    @Override
+    public String toString() {
+        return "Address [zip=" + zip.toString() + ", country=" + country + ", city=" + city +
+                ", district=" + district + ", street=" + street + "]";
+    }
 }

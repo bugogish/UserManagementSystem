f66b2598b09d02bef013454d62b84665f8c0d763
bugogish <ludlap@yandex.ru>
2017-07-27 17:51:16 +0300

    repository updated

--- src/main/java/com/ROI/test/model/UserRepository.java
+++ src/main/java/com/ROI/test/model/UserRepository.java
@@ -2,9 +2,14 @@ package com.ROI.test.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
+
+    Collection<User> findByBirthday(Date birthday);
+
+    Optional<User> findByEmail(String email);
 }
 

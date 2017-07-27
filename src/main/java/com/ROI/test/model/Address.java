6b7f9b8fc8ce3ea1da1fcc4be2309666b132214a
bugogish <bugogish@gmail.com>
2017-07-27 15:51:33 +0300

    model added

--- src/main/java/com/ROI/test/model/Address.java
+++ src/main/java/com/ROI/test/model/Address.java
@@ -1,4 +1,6 @@
 package com.ROI.test.model;
 
+//        Address is {zip, country, city, district, street}
+
 public class Address {
 }
--- src/main/java/com/ROI/test/model/User.java
+++ src/main/java/com/ROI/test/model/User.java
@@ -1,12 +1,13 @@
 package com.ROI.test.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+import java.sql.Date;
 
 //User {id, firsname, lastname, username, password, email, birthday, isActive, createdTimestamp,
 //        lastUpdatedTimestamp, address}
-//        Address is {zip, country, city, district, street}
 
 @Entity
 public class User {
@@ -14,5 +15,29 @@ public class User {
     @GeneratedValue
     private Long id;
 
+    private String firstName;
 
+    private String lastName;
+
+    private String userName;
+
+    @JsonIgnore
+    private String password;
+
+    private Email email; //EMAIL??? hybernate
+
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+
+    private boolean isActive;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTimestamp;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdatedTimestamp;
+
+    //Can we do it as a separate class?
+    private Address address;
 }
+

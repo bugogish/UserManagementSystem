82f293bec7026a7c23ae0d66f0b071e41d82cd01
bugogish <ludlap@yandex.ru>
2017-07-28 16:49:43 +0300

    added basic new user registration

--- src/main/java/com/ROI/test/controller/MainPageController.java
+++ src/main/java/com/ROI/test/controller/MainPageController.java
@@ -1,6 +1,5 @@
 package com.ROI.test.controller;
 
-import com.ROI.test.model.User;
 import com.ROI.test.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/ROI/test/controller/UserController.java
+++ src/main/java/com/ROI/test/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -54,26 +51,19 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/new_user", method = RequestMethod.GET)
-    public String createUser(Model model) {
-        model.addAttribute("userForm", new User());
+    @GetMapping("/new_user")
+    public String serveNewUserPage(Model model) {
+        model.addAttribute("user", new User());
 
         return "new_user";
     }
 
-    @RequestMapping(value = "/new_user", method = RequestMethod.POST)
-    public String createUser(@ModelAttribute("userForm") User userForm,
-                                           BindingResult bindingResult,
-                                           Model model) {
-//            userValidator.validate(userForm, bindingResult);
+    @PostMapping("/new_user")
+    public String createUser(@ModelAttribute User user) {
+        System.out.println(user.getUserName());
 
-        System.out.println(userForm.getUserName());
-            if (bindingResult.hasErrors()) {
-                return "new_user";
-            }
+        userRepository.save(user);
 
-            userRepository.save(userForm);
-
-            return "redirect:/ums";
+        return "/new_user";
     }
 }

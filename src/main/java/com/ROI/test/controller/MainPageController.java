d651a9284f6f5eaf9a9c138006ae7bc38d9954e5
bugogish <ludlap@yandex.ru>
2017-07-28 15:24:40 +0300

    SINGLE user search implemented (at /ums page)

--- src/main/java/com/ROI/test/controller/MainPageController.java
+++ src/main/java/com/ROI/test/controller/MainPageController.java
@@ -1,10 +1,17 @@
 package com.ROI.test.controller;
 
+import com.ROI.test.model.User;
 import com.ROI.test.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class MainPageController {
@@ -16,8 +23,14 @@ public class MainPageController {
     }
 
     @GetMapping({"/ums", "/"})
-    public String users(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+    public String searchUsersByUsername(@RequestParam(name = "username", required=false) String userName, Model model) {
+        if (userName != null) {
+            User user = userRepository.findByUserName(userName).orElse(null);
+            model.addAttribute("users", user);
+        }
+        else {
+            model.addAttribute("users", userRepository.findAll());
+        }
         return "ums";
     }
 }
